ccf540fb37b758c8dd3ec69e2303097c8e709335
Martin Fixman <martinfixman@gmail.com>
2016-07-10 14:37:37 -0300

    Agregando headers de simuladores.

---
+++ tp1/src/simuladores.h
@@ -0,0 +1,46 @@
+#ifndef SIMULADORES_H
+#define SIMULADORES_H
+
+#include "clases.h"
+
+class SimuladorPartido
+{
+	Monitor monitor;
+	public:
+
+	Monitor& darMonitor();
+
+	Monitor simular(
+		const Equipo& unEquipo,
+		const Equipo& otroEquipo
+	);
+};
+
+class SimuladorTurno
+{
+	int cadenaPases;
+	// Monitor monitor;
+
+	public:
+	void simular(
+		const Equipo& unEquipo,
+		const Equipo& otroEquipo,
+		Monitor& unMontor
+	);
+
+	void simularJugada(
+		const Equipo& unEquipo,
+		const Equipo& otroEquipo,
+		const JugadaOfensiva unaJugadaO,
+		const JugadaDefensiva unaJugadaD,
+	);
+
+	void agregarPase();
+
+	void simularPelotaDividida(
+		const Equipo& unEquipo,
+		const Equipo& otroEquipo
+	);
+};
+
+#endif
