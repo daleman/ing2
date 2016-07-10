22871570076efab7cc1980d99bb40ef33ee1d5d4
Martin Fixman <martinfixman@gmail.com>
2016-07-10 11:47:14 -0300

    Participante.

--- tp1/src/desafio.h
+++ tp1/src/desafio.h
@@ -6,7 +6,31 @@ class Cuenta
 	string email;
 	string contrasena;
 
-	bool validar(const string &) const;
+	public:
+	bool validar(string unaContrasena) const;
+};
+
+class Participante
+{
+	string nombre;
+	int fichas;
+	int cap;
+
+	public:
+	Equipo crearEquipo(
+		string unNombre,
+		const Tecnico& unTecnico,
+		const Jugador& jugadorBase,
+		const Jugador& jugadorEscolta,
+		const Jugador& jugadorAlero,
+		const Jugador& jugadorAlaPivot,
+		const Jugador& jugadorPivot
+	);
+
+	void procesarResultado(
+		const Monitor& unMonitor,
+		string unNombreEquipo
+	);
 };
 
 #endif
