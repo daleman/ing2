01d062a008330a036cd42e80ae6739fc6d24b41b
Martin Fixman <martinfixman@gmail.com>
2016-07-10 13:22:01 -0300

    Jugadores, Tecnico, y otras cosas.

---
+++ tp1/src/clases.h
@@ -0,0 +1,13 @@
+#ifndef CLASES_H
+#define CLASES_H
+
+class Cuenta;
+class Participante;
+class Plantel;
+class Equipo;
+class PlanillaDesafios;
+class Desafio;
+class TablaPosiciones;
+class Monitor;
+
+#endif
--- tp1/src/desafio.h
+++ tp1/src/desafio.h
@@ -1,6 +1,16 @@
 #ifndef DESAFIO_H
 #define DESAFIO_H
 
+#include <set>
+#include <string>
+#include <vector>
+
+#include "clases.h"
+
+using std::set;
+using std::string;
+using std::vector;
+
 class Cuenta
 {
 	string email;
@@ -153,6 +163,86 @@ class Monitor
 	Monitor(
 		const Equipo& unEquipo,
 		const Equipo& otroEquipo
+	);
+};
+
+class Jugador
+{
+	string nombre;
+	float fgpt, tppt, rpg, apt, bpg, spg, to, ppg;
+
+	public:
+	string darNombre() const;
+
+	float darFgpt() const;
+	float darTppt() const;
+	float darRpg() const;
+	float darApt() const;
+	float darBpg() const;
+	float darSpg() const;
+	float darTo() const;
+	float darPpg() const;
+
+	Jugador(
+		string nombre,
+		float fgpt,
+		float tppt,
+		float rpg,
+		float apt,
+		float bpg,
+		float spg,
+		float to,
+		float ppg
+	);
+};
+
+class Tecnico
+{
+	const string nombre;
+
+	const vector <PreferenciaOfensiva> tacticasOfensivas;
+	const vector <PreferenciaDefendiva> tacticasDefensivas;
+
+	public:
+	string darNombre() const;
+
+	const EstrategiaOfensiva& elegirEstrategiaOfensiva() const;
+	const EstrategiaDefensiva& elegirEstrategiaDefensiva() const;
+
+	Tecnico(
+		string nombre,
+		vector <PreferenciaOfensiva> tacticasOfensivas,
+		vector <PreferenciaDefendiva> tacticasDefensivas
+	);
+};
+
+class Preferencia
+{
+	int peso;
+
+	public:
+	Preferencia() = delete;
+};
+
+class PreferenciaOfensiva : public Preferencia
+{
+	public:
+	const EstrategiaOfensiva estrategia;
+
+	PreferenciaOfensiva(
+		int peso,
+		EstrategiaOfensiva estrategia
+	);
+};
+
+class PreferenciaDefensiva : public Preferencia
+{
+	public:
+	const EstrategiaDefensiva estrategia;
+
+	PreferenciaDefensiva(
+		int peso,
+		EstrategiaDefensiva estrategia
 	);
 };
 
